d8029c7ce4aeb58926c8a72d8404d11bc85e1664
Julie <J-Buma@hotmail.com>
2018-03-29 00:01:20 +0200

    Small Changes

--- myproject/src/components/Hello.vue
+++ myproject/src/components/Hello.vue
@@ -1,6 +1,9 @@
 <template>
 	<div>
 		<banner>Hello I am hello</banner>
+		<section id='intro'>
+			<code>This is a trello clone, frontend made with vue, backend made with nodeJs.</code>
+		</section>
 	</div>
 </template>
 <script>

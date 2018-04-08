0b545c90814f0fceb4c239883eb7d31a32244a5f
Julie <J-Buma@hotmail.com>
2018-04-08 15:38:20 +0200

    Put the currently selected item in state instead of in the project component

--- myproject/src/components/Project.vue
+++ myproject/src/components/Project.vue
@@ -35,67 +35,7 @@
 		<!-- Show The currently selected task, currently only show the name and description -->
 		<!-- TODO: Make into its own component -->
 		<!-- TODO: Put the currentitem in vuex state -->
-		<modal name='test' ref="show-task" id='show-task'>
-			<input type='text' slot="header" class='dark task editable name' v-model='currentItem.name' @change='updateTask("name")'/>
-			<div slot="body" class='modal-body'>
-				<div class="main">
-					<div class="description">
-						<textarea class='task editable description' v-model="currentItem.description" @change='updateTask("description")'></textarea>
-					</div>
-					<div class="comments">
-						<div class="new-comment"></div>
-						<div class="comment"></div>
-						<div class="comment"></div>
-						<div class="comment"></div>
-					</div>
-
-				</div>
-				<div class="info">
-					<h3>Categories</h3>
-					<div class="categories">
-						
-						<div class="category purple"></div>
-						<div class="category yellow"></div>
-						<div class="category green"></div>
-						<div class="category green"></div>
-						<div class="category green"></div>
-						<div class="category green"></div>
-						<div class="category green"></div>
-					</div>
-					<h3>Members</h3>
-					<div class="members">
-						
-						<div class="member">JB</div>
-						<div class="member">UD</div>
-						<div class="member">YM</div>
-					</div>
-					<h3>Due Date</h3>
-					<div class="due-date">
-						31 date at 12:00
-					</div>
-					<h3>Add</h3>
-					<div class="actions">
-						<ul>
-							<li>
-								<button>
-									Member
-								</button>
-							</li>
-							<li>
-								<button>
-									Category
-								</button>
-							</li>
-							<li>
-								<button>
-									Checklist
-								</button>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</modal>
+		<show-task ref='show-task' />
 	</section>
 
 </template>
@@ -136,9 +76,7 @@ export default {
 				this.$store.state.user.id,
 				this.$route.params.projectId,
 			);
-			// console.log(response);
 			this.projectInfo = response.data;
-			console.log(this.projectInfo);
 
 			const items = await itemServices.index(
 				this.$route.params.projectId,
@@ -146,6 +84,7 @@ export default {
 			this.items = items.data.itemList;
 		} catch (err) {
 			console.log(err);
+			alert(err, 'More info in console');
 		}
 	},
 	methods: {
@@ -167,16 +106,8 @@ export default {
 			this.$refs[name].closeModal();
 		},
 		showTask(task) {
-			this.currentItem = task;
-			this.$refs['show-task'].openModal();
 			this.$store.dispatch('setTask', task);
-		},
-		async updateTask(task) {
-			try {
-				await taskServices.update(this.currentItem);
-			} catch (err) {
-				alert(err);
-			}
+			this.$refs['show-task'].open();
 		},
 	},
 	computed: {},
@@ -201,147 +132,5 @@ export default {
 form.new-item {
 	display: flex;
 	flex-flow: column nowrap;
-}
-
-#show-task {
-	.modal-content {
-		min-width: 70vw;
-		min-height: 70vh;
-	}
-
-	.modal-body {
-		display: flex;
-		flex-flow: row nowrap;
-
-		.info {
-			flex-grow: 1;
-			// padding: $padding-medium;
-			// background-color: teal;
-			max-width: 175px;
-			display: flex;
-			flex-flow: column nowrap;
-			justify-content: flex-start;
-			// align-items: center;
-
-			& > h3 {
-				margin-top: $padding-huge;
-			}
-
-			& > .categories {
-				display: flex;
-				flex-flow: row wrap;
-
-				.category {
-					width: 50px;
-					height: 25px;
-					border-radius: 5px;
-					margin: $padding-small;
-
-					&.purple {
-						background-color: purple;
-					}
-					&.yellow {
-						background-color: yellow;
-					}
-					&.green {
-						background-color: greenyellow;
-					}
-				}
-			}
-
-			& > .members {
-				display: flex;
-				flex-flow: row wrap;
-
-				.member {
-					width: 50px;
-					height: 50px;
-					// padding: 16px;
-					background-color: grey;
-					border-radius: 50%;
-					display: flex;
-					justify-content: center;
-					align-items: center;
-				}
-			}
-
-			& > .actions {
-				// text-align: center;
-				display: flex;
-				flex-flow: column nowrap;
-
-				& > ul {
-					list-style: none;
-					padding: 0;
-					margin: 0;
-					display: flex;
-					flex-flow: column nowrap;
-
-					& > li {
-						margin-bottom: $padding-small;
-					}
-				}
-			}
-		}
-		.main {
-			flex-grow: 3;
-			padding-right: $padding-medium;
-		}
-	}
-
-	button {
-		font-size: 16px;
-		padding: 8px;
-		background-color: grey;
-		border-radius: 0;
-	}
-
-	input.editable {
-		border: none;
-	}
-	textarea.editable {
-		border: none;
-		padding: $padding-small;
-		resize: none;
-
-		&.description {
-			width: 100%;
-		}
-	}
-
-	.editable {
-		box-sizing: border-box;
-		background-color: transparent;
-
-		transition: 0.2s ease-out;
-
-		&.name {
-			font-size: $text-large;
-			font-weight: bold;
-		}
-
-		&.dark {
-			color: $text-alt;
-		}
-
-		&:hover {
-			cursor: pointer;
-		}
-
-		&:focus {
-			background-color: $color-background-smokey;
-
-			border-radius: 5px;
-
-			&.dark {
-				color: $text-primary;
-				background-color: white;
-			}
-
-			&:hover {
-				cursor: auto;
-			}
-		}
-	}
 }
 </style>
--- myproject/src/components/ui/showTask.vue
+++ myproject/src/components/ui/showTask.vue
@@ -1,87 +1,96 @@
 <template>
-	<modal name='test' ref="show-task" class='show-task'>
-		<h1 slot="header">{{task.name}} <input type="text" name="task-name" id="task-name" :value='task.name'>
-		</h1>
-		<div slot="body" class='modal-body'>
-			<div class="main">
-				<div class="description">
-					<p>
-						{{task.description}}
-					</p>
-				</div>
-				<div class="comments">
-					<div class="new-comment"></div>
-					<div class="comment"></div>
-					<div class="comment"></div>
-					<div class="comment"></div>
-				</div>
+	<div>
+		<modal name='task-modal' ref="task-modal" id='show-task'>
+			<input type='text' slot="header" class='dark task editable name' v-model='task.name' @change='updateTask("name")'/>
+			<div slot="body" class='modal-body'>
+				<!-- Main view, contains description and comments by team members -->
+				<div class="main">
+					<div class="description">
+						<!-- TODO: Maybe use markdown? Giving its' own editor with mini-modal -->
+						<textarea class='task editable description' v-model="task.description" @change='updateTask("description")'></textarea>
+					</div>
+					<div class="comments">
+						<h1>Comments</h1>
+						<div class="new-comment"></div>
+						<div class="comment"></div>
+						<div class="comment"></div>
+						<div class="comment"></div>
+					</div>
 
+				</div>
+				<div class="info">
+					<h3>Categories</h3>
+					<div class="categories">
+
+						<div class="category purple"></div>
+						<div class="category yellow"></div>
+						<div class="category green"></div>
+						<div class="category green"></div>
+						<div class="category green"></div>
+						<div class="category green"></div>
+						<div class="category green"></div>
+					</div>
+					<h3>Members</h3>
+					<div class="members">
+
+						<div class="member">JB</div>
+						<div class="member">UD</div>
+						<div class="member">YM</div>
+					</div>
+					<h3>Due Date</h3>
+					<div class="due-date">
+						31 date at 12:00
+					</div>
+					<h3>Add</h3>
+					<div class="actions">
+						<ul>
+							<li>
+								<button>
+									Member
+								</button>
+							</li>
+							<li>
+								<button>
+									Category
+								</button>
+							</li>
+							<li>
+								<button>
+									Checklist
+								</button>
+							</li>
+						</ul>
+					</div>
+				</div>
 			</div>
-			<div class="info">
-				<h3>Categories</h3>
-				<div class="categories">
-					
-					<div class="category purple"></div>
-					<div class="category yellow"></div>
-					<div class="category green"></div>
-					<div class="category green"></div>
-					<div class="category green"></div>
-					<div class="category green"></div>
-					<div class="category green"></div>
-				</div>
-				<h3>Members</h3>
-				<div class="members">
-					
-					<div class="member">JB</div>
-					<div class="member">UD</div>
-					<div class="member">YM</div>
-				</div>
-				<h3>Due Date</h3>
-				<div class="due-date">
-					31 date at 12:00
-				</div>
-				<h3>Add</h3>
-				<div class="actions">
-					
-					<ul>
-						<li>
-							<button>
-								Member
-							</button>
-						</li>
-						<li>
-							<button>
-								Category
-							</button>
-						</li>
-						<li>
-							<button>
-								Checklist
-							</button>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</modal>
+		</modal>
+	</div>
 </template>
 
 <script>
-import Modal from '../ui/modal.vue';
+import Modal from './modal.vue';
+import taskServices from '../../services/taskServices.js';
 export default {
 	components: {
 		Modal,
 	},
-	props: {
-		task: {
-			type: Object,
-			required: true,
-		},
+	data() {
+		return {
+			task: {},
+		};
 	},
 	methods: {
 		open() {
-			console.log(this.task);
-			this.$refs['show-task'].openModal();
+			this.task = this.$store.state.task;
+			this.$refs['task-modal'].openModal();
+		},
+		async updateTask(task) {
+			this.$store.dispatch('setTask', this.task);
+			try {
+				await taskServices.update(this.$store.state.task);
+			} catch (err) {
+				alert(err);
+			}
 		},
 	},
 };
@@ -89,7 +98,7 @@ export default {
 
 <style lang='scss'>
 @import '~vars';
-.show-task {
+#show-task {
 	.modal-content {
 		min-width: 70vw;
 		min-height: 70vh;
@@ -100,15 +109,12 @@ export default {
 		flex-flow: row nowrap;
 
 		.info {
-			flex-grow: 1;
-			// padding: $padding-medium;
+			flex-grow: 1; // padding: $padding-medium;
 			// background-color: teal;
 			max-width: 175px;
 			display: flex;
 			flex-flow: column nowrap;
-			justify-content: flex-start;
-			// align-items: center;
-
+			justify-content: flex-start; // align-items: center;
 			& > h3 {
 				margin-top: $padding-huge;
 			}
@@ -141,8 +147,7 @@ export default {
 
 				.member {
 					width: 50px;
-					height: 50px;
-					// padding: 16px;
+					height: 50px; // padding: 16px;
 					background-color: grey;
 					border-radius: 50%;
 					display: flex;
@@ -180,6 +185,54 @@ export default {
 		padding: 8px;
 		background-color: grey;
 		border-radius: 0;
+	}
+
+	input.editable {
+		border: none;
+	}
+	textarea.editable {
+		border: none;
+		padding: $padding-small;
+		resize: none;
+
+		&.description {
+			width: 100%;
+		}
+	}
+
+	.editable {
+		box-sizing: border-box;
+		background-color: transparent;
+
+		transition: 0.2s ease-out;
+
+		&.name {
+			font-size: $text-large;
+			font-weight: bold;
+		}
+
+		&.dark {
+			color: $text-alt;
+		}
+
+		&:hover {
+			cursor: pointer;
+		}
+
+		&:focus {
+			background-color: $color-background-smokey;
+
+			border-radius: 5px;
+
+			&.dark {
+				color: $text-primary;
+				background-color: white;
+			}
+
+			&:hover {
+				cursor: auto;
+			}
+		}
 	}
 }
 </style>
